a5975483c5ba36e43184d5619ce4cb770393e2d4
Tanel Marran <marran.tanel@gmail.com>
2024-01-09 16:34:21 +0200

    [main] refactors WobbleBackground

--- src/components/SingleImage.vue
+++ src/components/SingleImage.vue
@@ -1,8 +1,12 @@
 <template>
-  <img class="single-image h-image-full" :src="props.src" :alt="props.alt">
+  <div class="single-image">
+    <img class="single-image__img h-image-full" :src="props.src" :alt="props.alt">
+    <WiggleBackground class="single-image__background"/>
+  </div>
 </template>
 
 <script setup lang="ts">
+import WiggleBackground from '../components/WiggleBackground.vue'
 import {defineProps} from 'vue'
 
 const props = defineProps({
@@ -19,6 +23,18 @@ const props = defineProps({
 
 <style lang="scss">
 .single-image {
+  position: relative;
+}
+
+.single-image__img {
   border-radius: 4px;
+}
+
+.single-image__background {
+  left: -48px;
+  bottom: -64px;
+  width: 169px;
+  height: 128px;
+
 }
 </style>
